3d4ce34d78ac05e66e9ad9f3cee7032f96bb9bde
liuyang <xconfigurator@hotmail.com>
2022-05-26 19:45:08 +0800

    Apache HttpClient4 + Fastjson联用的工具类以及测试用例完成！还有一些感想.

---
+++ src/main/java/liuyang/testclienthttpclient4/modules/apache/httpclient4/utils/fastjson/package-info.java
@@ -0,0 +1,14 @@
+/**
+ * 2022/5/26 第一版完成
+ * 测试用例在liuyang.testclienthttpclient4.modules.apache.httpclient4.utils.fastjson.HttpClientUtilTest
+ * 看起来很容易，实现起来却问题层出。
+ * 工程实践，强迫症和“完美主义”要不得！
+ * 也许短期目标是用起来且可迭代（这也不简单），长期目标是满足需求趋近“完美”运行才是正确测略。
+ * 这绝不是功利，而是生存。弄懂这个道理，我竟然花了快40年，心理这砍儿真不好过！
+ * 不是我不想完美，我拼尽全力，然后发现，完美真的做不到，而且会坏事！
+ * 用起来，迭代吧，杨帆！  -- 202205261944 石家庄 鹿泉 信息产业园
+ *
+ * @author liuyang(wx)
+ * @since 2022/5/26
+ */
+package liuyang.testclienthttpclient4.modules.apache.httpclient4.utils.fastjson;
